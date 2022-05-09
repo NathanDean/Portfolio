e55ad7e323bce333263514deff677fa4b222aeaa
NathanDean <nathandean@hotmail.co.uk>
2022-05-09 19:37:14 +0100

    Added package.json and dependencies

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,34 +1,18 @@
-// Selects contact list, email h3 and form
-const email = document.querySelector("#emailLink");
-const form = document.querySelector("#formDiv");
-const contactList = document.querySelector("#contactList");
+const form = document.querySelector("form");
 
-// Records whether form is being displayed
-let isFormActive = false;
+function sendMail(mail){
+    fetch("https://nathandean.co.uk/contact.html", {
+        method:"POST",
+        body: mail
+    }).then(function(response){
+        return response.json();
+    })
+}
 
-// Records whether transition is in progress
-let isTransitionInProgress = false;
-
-// Toggles displaying of form on click
-email.addEventListener("click", function(){
-    // If transition is not already in progress, toggle displaying of form
-    if(!isTransitionInProgress){
-        isTransitionInProgress = !isTransitionInProgress;
-        contactList.classList.toggle("formActive");
-        if(isFormActive === false){
-            contactList.classList.toggle("formActiveWidth")
-            form.classList.toggle("active");
-            setTimeout(function(){
-                form.classList.toggle("show");
-                isTransitionInProgress = !isTransitionInProgress;
-            }, 1000);
-        } else {
-            form.classList.toggle("show");
-            setTimeout(function(){
-                form.classList.toggle("active");
-                isTransitionInProgress = !isTransitionInProgress;
-            }, 750);
-        }
-        isFormActive = !isFormActive;
-    }
+const formEvent = form.addEventListener("submit", function(event){
+    event.preventDefault();
 });
+
+let mail = new FormData(form);
+
+sendMail(mail);
